d1e8f7c7d827605b8a2fe25d88dde2ae2c1d12a3
Jacob House <jacob@jacobhouse.ca>
2019-01-17 17:58:35 -0330

    Moved zsh addons

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-ln -s $(HOME)/.dotfiles/.omz-addons/xxf/themes/xxf.zsh-theme $(HOME)/.dotfiles/.oh-my-zsh/custom/themes/xxf.zsh-theme
+ln -s $HOME/.dotfiles/.oh-my-zsh-addons/xxf/themes/xxf.zsh-theme $HOME/.dotfiles/.oh-my-zsh/custom/themes/xxf.zsh-theme
 
