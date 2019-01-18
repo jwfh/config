ba5c9dfb015d681b69f156f8cc630443abb4defb
Jacob House <jacob@jacobhouse.ca>
2019-01-18 13:37:03 -0330

    Added macOS wallpaper support

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,7 @@
 
 ln -s $HOME/.dotfiles/.oh-my-zsh-addons/xxf/themes/xxf.zsh-theme $HOME/.dotfiles/.oh-my-zsh/custom/themes/xxf.zsh-theme
 
+# Add custom wallpaper
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$HOME/.dotfiles/images/coffee.jpg'";
+killall Dock;
+
