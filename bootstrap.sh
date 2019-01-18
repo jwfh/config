4e00e67d8699110828fd4456888e0d2d11576e37
Jacob House <jacob@jacobhouse.ca>
2019-01-18 14:00:21 -0330

    Added Jekyll and HomeBrew

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env sh
 
+# Symlink our custom ZSH theme into the OMZ directory
 ln -s $HOME/.dotfiles/.oh-my-zsh-addons/xxf/themes/xxf.zsh-theme $HOME/.dotfiles/.oh-my-zsh/custom/themes/xxf.zsh-theme
 
 # Add custom wallpaper
 sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$HOME/.dotfiles/images/coffee.jpg'";
 killall Dock;
 
+# Install gem
+TMPA=$(mktemp -d)
+git clone --recurse https://github.com/rubygems/rubygems $(TMPA)
+cd $(TMPA)
+sudo ruby setup.rb
+
+# Install Jekyll static site generator
+gem install jekyll
+
+# Install HomeBrew - The missing package manager for macOS
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install the things that Apple didn't
+brew install wget npm fish 
