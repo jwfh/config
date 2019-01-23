650922009a59ae35c0a99ed7f28b2b9f704a1e54
Jacob William House <jwhouse@riemann.math.mun.ca>
2019-01-23 16:51:39 -0330

    Added many fixes and features

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
+set -e
+
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
-export CPPFLAGS="-I/usr/local/opt/openssl/include"
-export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/openssl-1.0.2h/include"
+export LDFLAGS="-L/usr/local/openssl-1.0.2h/lib"
 
 # Check if Xcode command line tools are installed.
 # If not, the user needs to install it manually. Apple has made scripting this impossible.
@@ -19,20 +21,57 @@ xcode-select --install &
 exit 1
 fi
 
+# Install gfortran
+TMPA=$(mktemp -d)
+cd "$TMPA"
+curl -OL "https://github.com/fxcoudert/gfortran-for-macOS/releases/download/8.2/gfortran-8.2-Mojave.dmg"
+hdiuril mount -nobrowse gfortran-8.2-Mojave.dmg -mountpoint "/Volumes/gfortran-8.2-Mojave"
+cd "/Volumes/gfortran-8.2-Mojave/gfortran-8.2-Mojave"
+sudo installer -verbose -pkg gfortran.pkg -target /
+cd "$HOME"
+diskutil umount "/Volumes/gfortran-8.2-Mojave"
+rm -rf "$TMPA"
+
+# Install JDK
+TMPA=$(mktemp -d)
+cd "$TMPA"
+curl -OL -H "Cookie: oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn-pub/java/jdk/11.0.2+9/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_osx-x64_bin.dmg"
+hdiutil mount -nobrowse jdk-11.0.2_osx-x64_bin.dmg -mountpoint /Volumes/jdk-11.0.2_osx-x64_bin
+cd /Volumes/jdk-11.0.2_osx-x64_bin
+sudo installer -verbose -pkg "JDK 11.0.2.pkg" -target /
+cd "$HOME"
+diskutil umount /Volumes/jdk-11.0.2_osx-x64_bin
+rm -rf "$TMPA"
+
 # Install cmake
 TMPA=$(mktemp -d)
 cd "$TMPA"
-curl -OL https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3.tar.gz
-tar -xzvf cmake-3.13.3.tar.gz
-cd cmake-3.13.3
-./bootstrap
-make
-make install
+curl -OL "https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3-Darwin-x86_64.dmg"
+hdiutil mount -nobrowse cmake-3.13.3-Darwin-x86_64.dmg -mountpoint /Volumes/cmake-3.13.3-Darwin-x86_64
+cp -rv /Volumes/cmake-3.13.3-Darwin-x86_64/CMake.app/ /Applications/CMake.app/
+sudo /Applications/CMake.app/Conents/bin/cmake-gui --install
+diskutil umount /Volumes/cmake-3.13.3-Darwin-x86_64
 cd "$HOME"
 rm -rf "$TMPA"
 
 # Clone the dotfiles repository
 git clone --recurse https://github.com/jwfh/dotfiles "$HOME/.dotfiles"
+# Symlink our custom ZSH theme into the OMZ directory
+ln -s "$HOME/.dotfiles/.oh-my-zsh-addons/xxf/themes/xxf.zsh-theme" "$HOME/.dotfiles/.oh-my-zsh/custom/themes/xxf.zsh-theme"
+# Create other necessary symlinks
+mkdir "$HOME/.config"
+ln -sf "$HOME/.dotfiles/.config/fish" "$HOME/.config/fish"
+ln -sf "$HOME/.dotfiles/.config/omf" "$HOME/.config/omf"
+mkdir -p "$HOME/.local/share"
+ln -sf "$HOME/.dotfiles/.local/share/fish" "$HOME/.local/share/fish"
+ln -sf "$HOME/.dotfiles/.local/share/omf" "$HOME/.local/share/omf"
+ln -sf "$HOME/.dotfiles/.vim" "$HOME/.vim"
+ln -s "$HOME/.dotfiles/.vimrc" "$HOME/.vimrc"
+ln -s "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/.dotfiles/.oh-my-zsh" "$HOME/.oh-my-zsh"
+ln -sf "$HOME/.dotfiles/.texrc" "$HOME/.texrc"
+ln -sf "$HOME/.dotfiles/.config/texstudio" "$HOME/.config/texstudio"
+cp "$HOME/.dotfiles/.latexmkrc" "$HOME/.latexmkrc"
 
 # Install OpenSSL from source
 TMPA=$(mktemp -d)
@@ -49,50 +88,47 @@ openssl version -a
 cd "$HOME"
 rm -rf "$TMPA"
 
-# Symlink our custom ZSH theme into the OMZ directory
-ln -s "$HOME/.dotfiles/.oh-my-zsh-addons/xxf/themes/xxf.zsh-theme" "$HOME/.dotfiles/.oh-my-zsh/custom/themes/xxf.zsh-theme"
-
 # Add custom wallpaper
-sips -Z $(system_profiler SPDisplaysDataType | awk '/Resolution/{print $2}') $HOME/.dotfiles/images/coffee.png --out $HOME/.dotfiles/images/coffee-out.png
-sips --cropToHeightWidth $(system_profiler SPDisplaysDataType | awk '/Resolution/{print $4, $2}') images/coffee-out.png
-sips -Z $(system_profiler SPDisplaysDataType | awk '/Resolution/{print $2}') $HOME/.dotfiles/images/coffee-blur.png --out $HOME/.dotfiles/images/coffee-blur-out.png
-sips --cropToHeightWidth $(system_profiler SPDisplaysDataType | awk '/Resolution/{print $4, $2}') images/coffee-blur-out.png
-# Make the loginwindowbgconverter binary and then use it to generate the lock screen image 
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$HOME/.dotfiles/images/coffee-out.png'";
-killall Dock;
+osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$HOME/.dotfiles/images/coffee.jpg\""
+
+# Install LZMA
+TMPA=$(mktemp -d)
+cd "$TMPA"
+curl -L "https://pilotfiber.dl.sourceforge.net/project/lzmautils/xz-5.2.4.tar.gz" -o "xz-5.2.4.tar.gz"
 
 # Install Python
 TMPA=$(mktemp -d)
 cd "$TMPA"
 curl -OL https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
 tar xzvf Python-3.7.2.tgz
-cd Python-3.7.2.tgz
+cd Python-3.7.2
 ./configure --prefix=/usr/local --enable-shared --enable-optimizations --with-openssl=/usr/local/openssl-1.0.2h/
 make
-make install
+sudo make install
 cd "$HOME"
 rm -rf "$TMPA"
 
 # Install useful Python modules
-python3 -m pip install numpy
-python3 -m pip install numba
-python3 -m pip install pyasn1
-python3 -m pip install pymysql
-python3 -m pip install cryptography
-python3 -m pip install dill
-python3 -m pip install Flask
-python3 -m pip install Flask-Login
-python3 -m pip install WTForms
-python3 -m pip install Flask-WTF
-python3 -m pip install itsdangerous
-python3 -m pip install Jinja2
-python3 -m pip install passlib
-python3 -m pip install Pillow
-python3 -m pip install pycups
-python3 -m pip install pysnmp
-python3 -m pip install requests
-python3 -m pip install urllib3
-python3 -m pip install Werkzeug
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install numpy
+sudo python3 -m pip install numba
+sudo python3 -m pip install pyasn1
+sudo python3 -m pip install pymysql
+sudo python3 -m pip install cryptography
+sudo python3 -m pip install dill
+sudo python3 -m pip install Flask
+sudo python3 -m pip install Flask-Login
+sudo python3 -m pip install WTForms
+sudo python3 -m pip install Flask-WTF
+sudo python3 -m pip install itsdangerous
+sudo python3 -m pip install Jinja2
+sudo python3 -m pip install passlib
+sudo python3 -m pip install Pillow
+sudo python3 -m pip install pycups
+sudo python3 -m pip install pysnmp
+sudo python3 -m pip install requests
+sudo python3 -m pip install urllib3
+sudo python3 -m pip install Werkzeug
 
 # Install Rust ($HOME/.cargo/bin should be in path from config/fish/config.fish)
 curl https://sh.rustup.rs -sSf | sh
@@ -105,17 +141,26 @@ sudo installer -verbose -pkg go1.11.4.darwin-amd64.pkg -target /
 cd "$HOME"
 rm -rf "$TMPA"
 
-
 # Install gem
 TMPA=$(mktemp -d)
 git clone --recurse https://github.com/rubygems/rubygems "$TMPA"
 cd "$TMPA"
 sudo ruby setup.rb
 cd "$HOME"
+test -f "/System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/bin/bundle" && sudo ln -s "/System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/bin/bundle" "/usr/local/bin/bundle"
 rm -rf "$TMPA"
 
 # Install Jekyll static site generator
-gem install jekyll
+sudo gem install jekyll
+
+# Install Hugo static site generator
+TMPA=$(mktemp -d)
+cd "$TMPA"
+curl -OL "https://github.com/gohugoio/hugo/releases/download/v0.53/hugo_0.53_macOS-64bit.tar.gz"
+tar -xvf hugo_0.53_macOS-64bit.tar.gz
+sudo mv hugo /usr/local/bin/
+cd "$HOME"
+rm -rf "$TMPA"
 
 # Install HomeBrew - The missing package manager for macOS
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -123,12 +168,23 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # Install the things that Apple didn't
 brew install wget
 brew install npm
-brew install fish
 brew install dot
 brew install doxygen
 brew install htop
 brew install golang
 brew install trombonehero/homebrew-grading/libgrading
+
+# Instal Fish, the friendly interactive shell
+TMPA=$(mktemp -d)
+cd "$TMPA"
+curl -OL "https://github.com/fish-shell/fish-shell/releases/download/3.0.0/fish-3.0.0.tar.gz"
+tar -xvf fish-3.0.0.tar.gz
+cd fish-3.0.0
+./configure
+make
+sudo make install
+cd "$HOME"
+rm -rf "$TMPA"
 
 # Use MacApps to install a number of useful applications
 curl -s 'https://macapps.link/en/firefox' | sh
