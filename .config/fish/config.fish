9e530e2d844e3c128024beb4b08a3d5c8d32a29d
jwfh <me@jwfh.ca>
2019-03-26 11:25:42 -0230

    Added CPPFLAGS and LDFLAGS to fishrc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,5 +13,29 @@ set -g CXX /usr/bin/clang++
 
 set -gx PATH $HOME/.cargo/bin $HOME/bin /opt/tools/anaconda3/bin $HOME/.dotfiles/bin $PATH
 
+
+set -gx PKG_CONFIG_PATH "/usr/local/opt/libffi/lib/pkgconfig" $PKG_CONFIG_PATH 
+
+# For compilers to find zlib you may need to set:
+set -gx LDFLAGS "-L/usr/local/opt/zlib/lib" $LDFLAGS
+set -gx CPPFLAGS "-I/usr/local/opt/zlib/include" $CPPFLAGS
+
+# For pkg-config to find zlib you may need to set:
+set -gx PKG_CONFIG_PATH "/usr/local/opt/zlib/lib/pkgconfig" $PKG_CONFIG_PATH 
+
+# For compilers to find icu4c you may need to set:
+set -gx LDFLAGS "-L/usr/local/opt/icu4c/lib" $LDFLAGS
+set -gx CPPFLAGS "-I/usr/local/opt/icu4c/include" $CPPFLAGS
+
+# For pkg-config to find icu4c you may need to set:
+set -gx PKG_CONFIG_PATH "/usr/local/opt/icu4c/lib/pkgconfig" $PKG_CONFIG_PATH
+
+# For compilers to find sqlite you may need to set:
+set -gx LDFLAGS "-L/usr/local/opt/sqlite/lib" $LDFLAGS
+set -gx CPPFLAGS "-I/usr/local/opt/sqlite/include" $CPPFLAGS
+
+# For pkg-config to find sqlite you may need to set:
+set -gx PKG_CONFIG_PATH "/usr/local/opt/sqlite/lib/pkgconfig" $PKG_CONFIG_PATH
+
 # Enable Vi key bindings
 fish_vi_key_bindings
